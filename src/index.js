a2778e5b2acc789cac84f8573789599dee7ef38c
Евгения Белоярцева <evgeniya.kornako@mail.ru>
2021-02-26 17:17:56 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+
+const element = (<div> 
+	Hello world
+	</div>);
+
+
+ReactDOM.render(element,document.getElementById('root'));
+
